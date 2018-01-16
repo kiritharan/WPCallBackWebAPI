007848a6095d4f8a4e1d3d83c4a4e8b47df0be35
kiritharan <kkiritharan@hotmail.com>
2018-01-16 16:45:18 -0500

    Update WebhookController.cs

--- MessengerBot/Controllers/WebhookController.cs
+++ MessengerBot/Controllers/WebhookController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,17 +11,26 @@ using System.Web.Http;
 using MessengerBot.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+
+using System.Xml;
 
 namespace MessengerBot.Controllers
 {
 	public class WebhookController : ApiController
 	{
-		//string pageToken = "DQVJ2SzVFMkpvNmM1QkpNZAUxrX1BMdUZAySW5uaWQ1UnZAkRGFsLU15aW1wZAkpwenNPTjNQSENIT2RBRGtNZAlNfbjZAkQ21vRmk4aTVpdEpGMlVoWl94ZAW4tNWpaMDgySXVqOTlaNnlyLWpfS3dvd3g5OEdnQUlKcnBLTzJxRFJ1dGZAkOVJreUI0b1o3U2NtS2FzUFprME5NcHl2YzNnTEo5a2JxRm9NRnV0MllGSWxkZATZA3a3BMUmZAXWk1BRUxqNjJmcTBMSlhkd2czOVRHTVdISAZDZD";
-		//string appSecret = "29e07098746b8ab7ec1b28210dbfe5ac";
+      
+        string app_ID = "150764529042457";
+        string appSecret = "c237e4905e1785299e4f22cdedcab84f";
+        string pageToken = "DQVJ0dnJoZA2VrcGNudk5oWEY5TEtnTlNpdWF2X2dXVWhYQ25Nb1h2Rm1KcmNidzVxNXFFSXZAYM3pLX3pDNHdDSlIxNEp0ZA01CMkIyU1VVSlVlOERRUVY4X1lVNmE0azVGRVRyY3c1ZA190cHZAxRWh3cEtvUUlKbFc1QWhTcjBlSjBnZAFpHNGlJNXBJNmxaS1VFYWhGdkwxQ0ZAwY3Y2Y2wwY21meE13WHpOeXQzbktKbXJzX0VuSUZAKUlRsTVlhNlN0ckxHOTlyRVktTzkzNFgxRgZDZD";
+
         // CIBC Real Time Monitoring App at cibc.facebook.com
-        string pageToken = "DQVJ2MjZAZAZA2tiMmhlNTB5S200SGRxbDdZASXZA4VHNuMVhuV3VnTTc5bTQtOE83QXFSSmRQN0g2WHRYV1R2eHJVbkd4MDNlbVNzVl93dm81YVhpaDFBNlN2YUN3SkRmdHpycGF4aWNtaXNPOWlLTWM3NGVCU05ISk5ZAdFFjSFl0S2RkS0pBanJVM0pfeDVvSnI5M1VWdld6SzJGbzgtc0NWSjFBYlJ3Rlh3QUVHNldFQTh0LVdFdDFNN2VVd1VaUm5DS3JLN3YxX2EwN295emtrNgZDZD";
-        string appSecret = "016aedf91eb245872fa87648da4b7b14";
-        string appI = "309396526132314";
+        //string pageToken = "DQVJ2MjZAZAZA2tiMmhlNTB5S200SGRxbDdZASXZA4VHNuMVhuV3VnTTc5bTQtOE83QXFSSmRQN0g2WHRYV1R2eHJVbkd4MDNlbVNzVl93dm81YVhpaDFBNlN2YUN3SkRmdHpycGF4aWNtaXNPOWlLTWM3NGVCU05ISk5ZAdFFjSFl0S2RkS0pBanJVM0pfeDVvSnI5M1VWdld6SzJGbzgtc0NWSjFBYlJ3Rlh3QUVHNldFQTh0LVdFdDFNN2VVd1VaUm5DS3JLN3YxX2EwN295emtrNgZDZD";
+        //string appSecret = "016aedf91eb245872fa87648da4b7b14";
+        //string appI = "309396526132314";
+
+        string localFilePath = "";
 
         public HttpResponseMessage Get()
 		{
@@ -44,77 +53,31 @@ namespace MessengerBot.Controllers
 			if (!VerifySignature(signature, body))
 				return new HttpResponseMessage(HttpStatusCode.BadRequest);
 
-			var value = JsonConvert.DeserializeObject<WebhookModel>(body);
+            AddingNodeToXmlFile(body);
 
-            
+           // foreach (var item in value.entry[0].messaging)
+           //{
 
-            if (value._object == "group")
-            {
-                var json = (JToken.Parse(body))["entry"][0]["changes"][0];
+            // if (item.message == null && item.postback == null)
+            //	continue;
+            //else
+            //	await SendMessage(GetMessageTemplate(item.message.text, item.sender.id));
+            //}
 
-                if (json["field"].ToString() == "posts")
-                {
-                    string from = json["value"]["from"].ToString();
-                    string type = json["value"]["type"].ToString();
-                    string target_type = json["value"]["target_type"].ToString();
-                    string community = json["value"]["community"].ToString();
-                    string post_id = json["value"]["post_id"].ToString();
-                    string verb = json["value"]["verb"].ToString();
-                    string created_time = json["value"]["created_time"].ToString();
-                    string message = json["value"]["message"].ToString();
-                    string permalink_url = json["value"]["permalink_url"].ToString();
-                }
-                else if (json["field"].ToString() == "comments")
-                {
-                    string from = json["value"]["from"].ToString();
-                    string type = json["value"]["type"].ToString();
-                    string target_type = json["value"]["target_type"].ToString();
-                    string community = json["value"]["community"].ToString();
-                    string post_id = json["value"]["post_id"].ToString();
-                    string verb = json["value"]["verb"].ToString();
-                    string created_time = json["value"]["created_time"].ToString();
-                    string message = json["value"]["message"].ToString();
-                    string permalink_url = json["value"]["permalink_url"].ToString();
-                }
-            }
-            else if (value._object == "user")
-            {
-                if (value._object == "message_sends")
-                {
-
-                }
-
-            }
-
-            if (value._object != "page")
-            {
-                //return new HttpResponseMessage(HttpStatusCode.OK);
-            }
-           
-           
-
-            foreach (var item in value.entry[0].messaging)
-			{
-				if (item.message == null && item.postback == null)
-					continue;
-				else
-					await SendMessage(GetMessageTemplate(item.message.text, item.sender.id));
-			}
-
-			return new HttpResponseMessage(HttpStatusCode.OK);
+            return new HttpResponseMessage(HttpStatusCode.OK);
 		}
 
 		private bool VerifySignature(string signature, string body)
-		{
-			var hashString = new StringBuilder();
+		{          
+            var hashString = new StringBuilder();
 			using (var crypto = new HMACSHA1(Encoding.UTF8.GetBytes(appSecret)))
 			{
 				var hash = crypto.ComputeHash(Encoding.UTF8.GetBytes(body));
 				foreach (var item in hash)
 					hashString.Append(item.ToString("X2"));
 			}
-
-			return hashString.ToString().ToLower() == signature.ToLower();
+            
+            return hashString.ToString().ToLower() == signature.ToLower();
 		}
 
 		/// <summary>
@@ -144,76 +107,163 @@ namespace MessengerBot.Controllers
 				HttpResponseMessage res = await client.PostAsync($"https://graph.facebook.com/v2.6/me/messages?access_token={pageToken}", new StringContent(json.ToString(), Encoding.UTF8, "application/json"));
 			}
 		}
-	}
+
+        #region "Private functions"
+
+        private void AddingNodeToXmlFile(string body)
+        {
+            try
+            {
+                var value = JsonConvert.DeserializeObject<WebhookModel>(body);
+                WebhookModel model = JsonConvert.DeserializeObject<WebhookModel>(body);
+                
+                model.entry[0].id = value.entry[0].id;
+                model.entry[0].uid = value.entry[0].uid;
+                model.entry[0].time = value.entry[0].time;
+              
+                if ((value._object == "group") || (value._object == "user"))
+                {
+                    var json = (JToken.Parse(body))["entry"][0]["changes"][0];
+                    model.field = json["field"].ToString();
+                  
+                    if ((json["field"].ToString() == "posts") || (json["field"].ToString() == "comments"))
+                    {                       
+                        Post post = JsonConvert.DeserializeObject<Post>(json["value"].ToString());
+
+                        Trace.TraceInformation("model1qweq2: " + post.community.ToString());
+                        Trace.TraceInformation("model1qweq3: " + post.created_time.ToString());
+                        Trace.TraceInformation("model1qweq4: " + post.post_id.ToString());
+                        Trace.TraceInformation("model1qweq5: " + post.verb.ToString());
+                        Trace.TraceInformation("model1qweq6: " + post.message.ToString());
+                        Trace.TraceInformation("model1qweq7: " + post.from.ToString());
+                        Trace.TraceInformation("model1qweq8: " + post.permalink_url.ToString());
+                        try
+                        {  
+                            model.entry[0].AddPost(post);
+
+                      
+
+                        Trace.TraceInformation("model1: " + model.field.ToString());
+                        Trace.TraceInformation("model1: " + model.entry[0].Post[0].type.ToString());
+                        Trace.TraceInformation("model1: " + model.entry[0].Post[0].community.ToString());
+                        Trace.TraceInformation("model1: " + model.entry[0].Post[0].created_time.ToString());
+                        Trace.TraceInformation("model1: " + model.entry[0].Post[0].post_id.ToString());
+                        Trace.TraceInformation("model1: " + model.entry[0].Post[0].verb.ToString());
+                        Trace.TraceInformation("model1: " + model.entry[0].Post[0].message.ToString());
+                        Trace.TraceInformation("model1: " + model.entry[0].Post[0].from.ToString());
+                        Trace.TraceInformation("model1: " + model.entry[0].Post[0].permalink_url.ToString());
+                        }
+                        catch (Exception ex) { Trace.TraceError(ex.ToString()); }
+                    }
+
+                    else if (value._object == "user")
+                    {
+                        if (json["field"].ToString() == "message_sends")
+                        {
+                            Trace.TraceInformation("Inside message: ");
+                            Message msg = JsonConvert.DeserializeObject<Message>(json["value"].ToString());
+                            //Message msg = new Message();
+                            //msg.created_time = json["value"]["created_time"].ToString();
+                            //msg.from = JsonConvert.DeserializeObject<User>(json["value"]["from"].ToString());
+                            ////Trace.TraceInformation("to count: " + json["value"]["to"]["data"].Count().ToString());
+                            //List<Recipient> recipients = new List<Models.Recipient>();
+                            //for (int i = 0; i < json["value"]["to"]["data"].Count(); i++)
+                            //{
+                            //    //Trace.TraceInformation("to: " + json["value"]["to"]["data"][i]["name"].ToString());
+                            //    recipients.Add(JsonConvert.DeserializeObject<Recipient>(json["value"]["to"]["data"][i].ToString()));
+                            //}
+
+                            //msg.to = recipients;
+                            //msg.message = json["value"]["message"].ToString();
+                            //msg.id = json["value"]["id"].ToString();
+                            try { 
+                            model.entry[0].Messages.Add(msg);
+                            }
+                            catch (Exception ex) { Trace.TraceError(ex.ToString()); }
+
+                           
+                            Trace.TraceInformation("model1: " + model.field.ToString());
+                            Trace.TraceInformation("model1: " + model.entry[0].Messages[0].created_time.ToString());
+                            Trace.TraceInformation("model1: " + model.entry[0].Messages[0].from.ToString());
+                            Trace.TraceInformation("model1: " + model.entry[0].Messages[0].to.ToString());
+                            Trace.TraceInformation("model1: " + model.entry[0].Messages[0].message.ToString());
+                            Trace.TraceInformation("model1: " + model.entry[0].Messages[0].id.ToString());
+                        }
+                    }
+                    else if (value._object == "page")
+                    {
+                        //return new HttpResponseMessage(HttpStatusCode.OK);
+                    }
+                }
+
+                //  XmlDocument doc = CheckForFile();
+
+                // AddToXMLFile(doc);
+
+                Trace.TraceInformation("** END **");
+            }
+            catch(Exception ex) { }
+        }
+
+        /// <summary>
+        /// This method will check file existense, such as if a file is already created and exists for today ignore creating a new file otherwise create a new file.
+        /// </summary>
+        private XmlDocument CheckForFile()
+        {
+            //XML for filtered chat conversations
+            XmlDocument docFiltered = new XmlDocument();
+            XmlDeclaration xDeclareFiltered = docFiltered.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement documentRootFiltered = docFiltered.DocumentElement;
+            docFiltered.InsertBefore(xDeclareFiltered, documentRootFiltered);
+            XmlElement filteredUsersElement = (XmlElement)docFiltered.AppendChild(docFiltered.CreateElement("Users"));
+
+            //XML for unfiltered chat conversations
+            XmlDocument doc = new XmlDocument();
+            XmlDeclaration xDeclare = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement documentRoot = doc.DocumentElement;
+            doc.InsertBefore(xDeclare, documentRoot);
+            XmlElement usersElement = (XmlElement)doc.AppendChild(doc.CreateElement("Users"));
+
+            return doc;
+        }
+        private void CreateXMLFile()
+        {
+                      
+        }
+
+
+        private void AddToXMLFile(XmlDocument doc)
+        {
+            try
+            {
+                XmlElement userElement = null;
+                XmlElement conversationsElement = null;
+                XmlElement filteredUserElement = null;
+                XmlElement filteredConversationsElement = null;
+                XmlElement exceptionElement = null;
+
+
+
+                //userElement = (XmlElement)usersElement.AppendChild(doc.CreateElement("User"));
+                //userElement.SetAttribute("WorkplaceUserID", user.id);
+                //userElement.SetAttribute("UserName", user.name);
+                //conversationsElement = (XmlElement)userElement.AppendChild(doc.CreateElement("Conversations"));
+
+
+                //filteredUserElement = (XmlElement)filteredUsersElement.AppendChild(docFiltered.CreateElement("User"));
+                //filteredUserElement.SetAttribute("WorkplaceUserID", user.id);
+                //filteredUserElement.SetAttribute("UserName", user.name);
+                //filteredConversationsElement = (XmlElement)filteredUserElement.AppendChild(docFiltered.CreateElement("Conversations"));
+
+
+                //conversationElement = (XmlElement)conversationsElement.AppendChild(doc.CreateElement("Conversation"));
+                //conversationElement.SetAttribute("WorkChatID", json.data[i]["id"].Value);
+
+            }
+            catch(Exception ex) { }
+        }
+
+        #endregion
+    }
 }
 
-/*
- {"entry": 
-[{"changes": 
-[{"field": "posts", 
-"value": {"from": {"id": "100016022019645", "name": "Kiritharan Kanesapillai"}, 
-"type": "status", "target_type": "group", 
-"community": {"id": "1206397592809948"}, 
-"post_id": "483499012017824_545236315844093", 
-"verb": "add", 
-"created_time": "2018-01-16T02:59:58+0000", 
-"message": "iyiiyuiuiu", 
-"permalink_url": "https://cibc.facebook.com/groups/483499012017824/permalink/545236315844093/"}}], 
-"id": "483499012017824", 
-"time": 1516071600}], 
-"object": "group"}
-
-
-{"entry": 
-[{"changes": 
-[{"field": "comments", 
-"value": {"from": {"id": "100016022019645", "name": "Kiritharan Kanesapillai"}, 
-"comment_id": "545236832510708", 
-"community": {"id": "1206397592809948"}, 
-"post_id": "483499012017824_545236315844093", 
-"verb": "add", 
-"created_time": "2018-01-16T03:03:13+0000",
- "message": "this is comment", 
-"permalink_url": "https://cibc.facebook.com/groups/483499012017824/permalink/545236315844093/?comment_id=545236832510708"}}], 
-"id": "483499012017824", 
-"time": 1516071794}],
- "object": "group"}
-
-
-{"entry": 
-[{"time": 1516071939, 
-"changes": [{"field": "message_sends", 
-"value": {"created_time": "2018-01-16T03:05:39+0000", 
-"to": {"data": [{"id": "100015950115290", "name": "Lahiru Pinnaduwage", 
-"community": {"id": "1206397592809948"}, 
-"email": "lahiru.pinnaduwage@cibc.com"}]}, 
-"message": "fdfdsffs", 
-"from": {"id": "100016022019645", "name": "Kiritharan Kanesapillai", 
-"community": {"id": "1206397592809948"}, 
-"email": "kiritharan.kanesapillai@cibc.com"}, 
-"id": "m_mid.$cAAAAAAxbb-dnLjM2Mlg_O1EUcVB7"}}], 
-"id": "100016022019645", 
-"uid": "100016022019645"}], 
-"object": "user"}
-
-
-
-
-{"entry": 
-[{"changes": [{"field": "posts", 
-"value": {"from": {"id": "100016022019645", "name": "Kiritharan Kanesapillai"}, 
-"attachments": {"data": [{"target": {"url": "https://lookaside.fbsbx.com/file/Microsoft.AspNet.WebApi.5.2.3.nupkg?token=AWxyec5Jh3XK8wUZQAXSH5zjQGNiuk5P02GMrqi776yC3SjJsZxXHD3AowJcWt8fXNsWVor_OUX1SbJtVqh5PMIN_Foti-Ot0BtkDwbYCK9PAk-aThIQyD8PLNfahR5jOTnHnfYuS6LsQZRFhf5n9vH0luX2Qdm3k5mY7Z-jmPRcnrRdNtuXwv8Etn4bLC7tyE_t0ZipCuybIv5WKcnFUy2ltVkZBeQOwPFgPq6jb9ue9g"}, "title": "Microsoft.AspNet.WebApi.5.2.3.nupkg \u00b7 version 1", 
-"url": "https://lookaside.fbsbx.com/file/Microsoft.AspNet.WebApi.5.2.3.nupkg?token=AWxyec5Jh3XK8wUZQAXSH5zjQGNiuk5P02GMrqi776yC3SjJsZxXHD3AowJcWt8fXNsWVor_OUX1SbJtVqh5PMIN_Foti-Ot0BtkDwbYCK9PAk-aThIQyD8PLNfahR5jOTnHnfYuS6LsQZRFhf5n9vH0luX2Qdm3k5mY7Z-jmPRcnrRdNtuXwv8Etn4bLC7tyE_t0ZipCuybIv5WKcnFUy2ltVkZBeQOwPFgPq6jb9ue9g", 
-"media": {"image": {"src": "https://static.xx.fbcdn.net/rsrc.php/v3/yk/r/VCodXOH-RMH.png", "width": 72, "height": 72}}, 
-"subattachments": {"data": [{"url": "https://cibc.facebook.com/groups/483499012017824/545237825843942/", 
-"type": "album", "target": {"url": "https://cibc.facebook.com/groups/483499012017824/545237825843942/"}, 
-"title": "Photos from Kiritharan Kanesapillai's post in Testing Workplace"}]}, "type": "file_upload"}]}, 
-"type": "status", "target_type": "group", "community": {"id": "1206397592809948"}, 
-"post_id": "483499012017824_545237825843942", 
-"verb": "add", 
-"created_time": "2018-01-16T03:07:45+0000",
- "permalink_url": "https://cibc.facebook.com/groups/483499012017824/545237825843942/"}}], 
-"id": "483499012017824", 
-"time": 1516072067}], 
-"object": "group"}
-     */
