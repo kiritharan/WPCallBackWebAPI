66490048cae98a53b1e19604a9283b3af3723695
kiritharan <kkiritharan@hotmail.com>
2018-01-18 15:34:58 -0500

    Update WebhookModel.cs

--- MessengerBot/Models/WebhookModel.cs
+++ MessengerBot/Models/WebhookModel.cs
@@ -18,17 +18,11 @@ namespace MessengerBot.Models
         public long time { get; set; }
         public string uid { get; set; }
         public Message Message { get; set; }
-        public Post Post { get; set; }
-      //  public List<Change> Changes { get; set; }
+        public Post Post { get; set; }      
+        public List<Attachment> attachments { get; set; }
     }
 
-    //public class Change
-    //{
-    //    public string field { get; set; }
-    //    public Post Post { get; set; }
-    //   // public AttachmentColl attachments { get; set; }
-    //}
-
+ 
     public class Post
     {
         public Post() { }
@@ -39,34 +33,39 @@ namespace MessengerBot.Models
         public string verb { get; set; }
         public string created_time { get; set; }
         public string message { get; set; }
-        public string permalink_url { get; set; }
-        public AttachmentColl attachments { get; set; }
-        // public string target_type { get; set; }
-        // public string community_id { get; set; }
-    }
-
-    public class AttachmentColl
-    {
-        public List<Attachment> data { get; set; }
+        public string permalink_url { get; set; }       
+        
     }
 
     public class Attachment
     {
-        public string description { get; set; }
-
-        public string target { get; set; }
+        public string description { get; set; }       
         public string type { get; set; }
         public string url { get; set; }
+        public string src { get; set; }
+        public string title { get; set; }
 
+        // Following for attachments in message
+        public string preview { get; set; }
+        public string size { get; set; }
+        public string id { get; set; }
+        public string mime_type { get; set; }
+        public string name { get; set; }
+       
+    }
+
+    public class MessageAttachment
+    {
+        public List<Attachment> data { get; set; }
     }
     public class Message
     {
         public User from { get; set; }
-        public List<Recipient> to { get; set; }
+        public Recipient to { get; set; }
         public string created_time { get; set; }
         public string message { get; set; }
-        public string messageId { get; set; }
         public string id { get; set; }
+        public MessageAttachment attachments { get; set; }
     }
 
     public class User
