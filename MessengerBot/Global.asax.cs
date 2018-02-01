03f034f89e7ad7698dc48769917939931b94c5a1
kiritharan <kkiritharan@hotmail.com>
2018-02-01 15:23:31 -0500

    Update Global.asax.cs

--- MessengerBot/Global.asax.cs
+++ MessengerBot/Global.asax.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+
+using System.Web.Mvc;
 
 namespace MessengerBot
 {
@@ -12,6 +14,19 @@ namespace MessengerBot
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
+            routes.MapRoute(
+                "Manage",
+                "Manage/{action}/{id}", // go to MVC controller
+             defaults: new { controller = "Manage", action = "Index", id = RouteParameter.Optional }
+           );
         }
     }
 }
